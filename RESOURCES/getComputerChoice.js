dc3c3a60e887e74e12bc3a6d08f9d06c25973679
AfoDy <afodydigitals@gmail.com>
2023-09-21 20:55:58 -0700

    refactor functions and split them into files linking them using modules

---
+++ RESOURCES/getComputerChoice.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")(); 
+let choices = ["Rock", "Paper", "Scissors"];
+
+export default function getComputerChoice(){
+    let choice= Math.floor(Math.random()*choices.length);
+    console.log("Computer picked" + " " + choices[choice]);
+    return choices[choice];
+};
+// getComputerChoice();
